fa9f409db522e81068b21498657246de005a1f00
Kushagra910 <kushagram63@gmail.com>
2023-07-21 22:16:11 +0530

    Entered Multiplayer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React, {useEffect} from "react";
+import React, {useEffect,useEffect} from "react";
 import Multiplayer from "./pages/Multiplayer";
 import Navbar from "./components/Navbar";
 import Solo from "./pages/Solo";
 import Home from './pages/Home'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,useNavigate } from "react-router-dom";
 import socket from './socket' ;
 
 function App(){
   
+  const [gameState,setGameState] = useState({_id:"",isOpen:false,players:[],words:[]});
+  const navigate = useNavigate();
   useEffect(() =>{
-    socket.on('test', (message) =>{
-      console.log(message);
-    });
+    // socket.on('test', (message) =>{
+    //   console.log(message);
+    // });
 
+    socket.on('updatGame',(game) =>{
+      console.log(game);
+      setGameState(game);
+    })
+
+    // return ()=>{
+    //   socket.removeAllListeners();
+    // }
     return () => {
       socket.off('test');
     };
   }, []);
+
+  useEffect(()=>{
+    if(gameState._id !== ""){
+       navigate(`/game/${gameState._id}`);
+    }
+  },[gameState._id]);
 
   return (
       <div className="w-full h-[100vh]">
--- src/pages/Multiplayer.jsx
+++ src/pages/Multiplayer.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import CreateGame from '../components/CreateGame';
+import JoinGame from '../components/JoinGame';
 
 const Multiplayer = () => {
+  // const naviget = useNavigate();
   return (
-    <div>Multiplayer</div>
+    <div>
+
+    <NavLink to='/CreateGame'>
+        <div>Create Game</div>
+    </NavLink>
+    <NavLink to='/JoinGame'>
+        <button>Join Game</button>
+    </NavLink>
+    </div>
+    
   )
 }
 
